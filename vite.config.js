37d795d9ef9b2a9a462e6b174617352ffd07e964
kloeffler95 <kevin.loeffler@colorado.edu>
2024-11-23 21:14:45 -0700

    Configuring PWA so that it is installable in Chrome.

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,35 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'WordProblemWeaver',
+        short_name: 'WordProblemWeaver',
+        description: 'An AI powered, offline-first math practice app for Chrome',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
       }
     })
   ],
