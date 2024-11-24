7454ffa13fac5f4903fb980ed3ce14888a145fe3
kloeffler95 <kevin.loeffler@colorado.edu>
2024-11-23 21:20:03 -0700

    Moving HEAD tags to their proper place.

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <link rel="icon" href="/favicon.ico" sizes="48x48">
-    <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
